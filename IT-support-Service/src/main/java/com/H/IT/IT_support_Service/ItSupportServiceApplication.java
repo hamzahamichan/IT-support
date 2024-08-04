7f4b586eeeea7b130a6ad4897f75251f86e4105e
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-04 16:00:05 +0100

    Création de projet et l'ajoute des dependency nécessaire

---
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/ItSupportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.H.IT.IT_support_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSupportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSupportServiceApplication.class, args);
+	}
+
+}
---
+++ IT-support-Service/src/test/java/com/H/IT/IT_support_Service/ItSupportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.H.IT.IT_support_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSupportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
