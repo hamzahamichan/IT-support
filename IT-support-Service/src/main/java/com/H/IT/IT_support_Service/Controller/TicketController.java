5998df224189acff15fc68bbe213b23d61b36338
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-07 17:30:21 +0100

    Utilisation de Contrller Tickets

---
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Controller/TicketController.java
@@ -0,0 +1,23 @@
+package com.H.IT.IT_support_Service.Controller;
+
+
+import com.H.IT.IT_support_Service.Model.DTO.TicketSupportDto;
+import com.H.IT.IT_support_Service.Model.Entity.TicketSupport;
+import com.H.IT.IT_support_Service.Service.TicketSerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Ticket")
+public class TicketController {
+
+    @Autowired
+    TicketSerImpl ticketSer;
+@PostMapping("/add-ticket")
+    public TicketSupportDto addTicketSupport(@RequestBody TicketSupport ticketSupport){
+    return this.ticketSer.addTicketSupport(ticketSupport);
+}
+}
