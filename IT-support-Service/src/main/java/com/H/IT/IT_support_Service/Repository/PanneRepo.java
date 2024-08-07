8693c13e41f1892998bb047e7d619eb85f0ef6a8
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-07 17:31:32 +0100

    Création de Repo panne

---
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Repository/PanneRepo.java
@@ -0,0 +1,8 @@
+package com.H.IT.IT_support_Service.Repository;
+
+import com.H.IT.IT_support_Service.Model.Entity.Panne;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PanneRepo extends JpaRepository<Panne,Integer> {
+
+}
