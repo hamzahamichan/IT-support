4f1956a87f2d95876ea14df22bbb908f98cb570d
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-07 17:21:08 +0100

    création de repo Equipement

---
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Repository/EquipementRepository.java
@@ -0,0 +1,8 @@
+package com.H.IT.IT_support_Service.Repository;
+
+
+import com.H.IT.IT_support_Service.Model.Entity.Equipement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipementRepository extends JpaRepository<Equipement, Integer> {
+}
