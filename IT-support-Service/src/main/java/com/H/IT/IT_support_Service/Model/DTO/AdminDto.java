2990b48551b56ee20558b943631b0df2cdc249f6
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-07 11:47:37 +0100

    création de table Admin et son Dto

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Model/DTO/AdminDto.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Model/DTO/AdminDto.java
@@ -1,4 +1,25 @@
 package com.H.IT.IT_support_Service.Model.DTO;
 
-public class AdminDto extends PersonneDto{
+import com.H.IT.IT_support_Service.Model.Entity.Admin;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class AdminDto {
+    private int id_admin ;
+    private  String nom_complet;
+    private  String Email;
+    private String password;
+    private static AdminDto  toDto(Admin per){
+        return AdminDto .builder()
+                .id_admin(per.getId_admin())
+                .nom_complet(per.getNom_complet())
+                .Email(per.getEmail())
+                .password(per.getPassword())
+                .build();
+    }
 }
