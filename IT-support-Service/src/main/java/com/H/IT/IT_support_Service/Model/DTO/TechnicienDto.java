f2588fcf1090adbe406d3258c002295d8607465b
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-07 11:48:55 +0100

    création de Technisien Dto

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Model/DTO/TechnicienDto.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Model/DTO/TechnicienDto.java
@@ -1,4 +1,26 @@
 package com.H.IT.IT_support_Service.Model.DTO;
 
-public class TechnicienDto extends PersonneDto{
+import com.H.IT.IT_support_Service.Model.Entity.Technicien;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class TechnicienDto {
+    private int id_Tech;
+    private String nom_complet;
+    private String Email;
+    private String password;
+
+    private static TechnicienDto toDto(Technicien per) {
+        return TechnicienDto.builder()
+                .id_Tech(per.getId_Tech())
+                .nom_complet(per.getNom_complet())
+                .Email(per.getEmail())
+                .password(per.getPassword())
+                .build();
+    }
 }
