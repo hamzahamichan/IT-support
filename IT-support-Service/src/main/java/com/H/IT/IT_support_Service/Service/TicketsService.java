3438c5410d19ed778bfb9e828377cb8cdc101d41
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-08 10:30:53 +0100

    Ajouter des Méthodes

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Service/TicketsService.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Service/TicketsService.java
@@ -1,9 +1,8 @@
 package com.H.IT.IT_support_Service.Service;
 
-import com.H.IT.IT_support_Service.Model.DTO.TicketSupportDto;
 import com.H.IT.IT_support_Service.Model.Entity.TicketSupport;
 
 public interface TicketsService {
-    TicketSupportDto addTicketSupport(TicketSupport ticketSupport);
+    TicketSupport addTicketSupport(TicketSupport ticketSupport);
     void deleteTickets(Integer id_ticket);
 }
