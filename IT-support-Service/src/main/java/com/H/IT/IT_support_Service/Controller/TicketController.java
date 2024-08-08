cd0084408ce998f4ed96a7ba6d676e5d5eebb48f
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-08 16:57:20 +0100

    Utilisation des méthode services

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Controller/TicketController.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Controller/TicketController.java
@@ -1,23 +1,42 @@
 package com.H.IT.IT_support_Service.Controller;
 
 
-import com.H.IT.IT_support_Service.Model.DTO.TicketSupportDto;
 import com.H.IT.IT_support_Service.Model.Entity.TicketSupport;
 import com.H.IT.IT_support_Service.Service.TicketSerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/Ticket")
+@RequestMapping("/ticket")
 public class TicketController {
 
     @Autowired
     TicketSerImpl ticketSer;
-@PostMapping("/add-ticket")
-    public TicketSupportDto addTicketSupport(@RequestBody TicketSupport ticketSupport){
-    return TicketSupportDto.toDto(this.ticketSer.addTicketSupport(ticketSupport));
+@PostMapping("/addT")
+    public TicketSupport addTicketSupport(@RequestBody TicketSupport ticketSupport){
+    return this.ticketSer.addTicketSupport(ticketSupport);
 }
+
+@GetMapping("/get")
+    public List<TicketSupport> getAllTickets() {
+        return this.ticketSer.getAllTickets();
+    }
+    @DeleteMapping("/delete/{id_ticket}")
+    public void deleteTickets(@PathVariable int id_ticket){
+    this.ticketSer.deleteTickets(id_ticket);
+    }
+
+@GetMapping("/awid")
+    public TicketSupport getTicket(@RequestParam Integer id){
+    return this.ticketSer.getTicket(id);
+}
+
+@PutMapping("/assigne/{id}")
+    public TicketSupport linkTicket(@PathVariable Integer id,@RequestBody TicketSupport ticketSupport){
+    return this.ticketSer.linkTicket(id,ticketSupport);
+
+}
+
 }
