705b16fcedd1e58c16f08667155406f3f39aa0f6
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-08 10:26:03 +0100

    Ajouter des modifications

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Model/DTO/TicketSupportDto.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Model/DTO/TicketSupportDto.java
@@ -3,6 +3,7 @@ package com.H.IT.IT_support_Service.Model.DTO;
 import com.H.IT.IT_support_Service.Model.Entity.TicketSupport;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.Date;
 @Getter
 @Setter
@@ -13,19 +14,16 @@ import java.util.Date;
 public class TicketSupportDto {
     private int id_ticket;
     private String description;
-    private Date date_creation;
+    private LocalDate date_creation;
     private int id_Tech;
-    private int id_panne;
     private  String Etat ;
-    private int equipementId;
     private Date date_traitement;
 public static TicketSupportDto toDto(TicketSupport tick){
     return  TicketSupportDto.builder()
             .id_ticket(tick.getId_ticket())
-            .id_panne(tick.getPanne().getId_panne())
             .date_creation(tick.getDate_creation())
             .date_traitement(tick.getDate_traitement())
-            .Etat(tick.getEtat())
+            .Etat(tick.getEtat().name())
             .description(tick.getDescription())
             .build();
 }
