4510f574a76aa3c8920dcef777f822b65243f103
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-08 17:04:09 +0100

    Ajouter des les méthodes

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Service/EquipementService.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Service/EquipementService.java
@@ -4,11 +4,12 @@ import com.H.IT.IT_support_Service.Model.DTO.EquipementDto;
 import com.H.IT.IT_support_Service.Model.Entity.Equipement;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface EquipementService {
     EquipementDto addEquipement(EquipementDto equipementDto);
-    EquipementDto getEquipement(Integer id);
+    Optional<Equipement> getEquipement(Integer id);
     List<Equipement> getEquipements();
     void deleteEquipements(int id);
 }
