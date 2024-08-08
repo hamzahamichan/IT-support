33d24d7f8822e99b5743ada3cbaafcc3283978a4
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-08 17:04:50 +0100

    Compléter les fonctionnalités

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Service/PanneService.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Service/PanneService.java
@@ -7,6 +7,8 @@ import com.H.IT.IT_support_Service.Repository.PanneRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PanneService {
 
@@ -16,4 +18,13 @@ public class PanneService {
     public Panne addPanne(Panne panne){
       return this.panneRepo.save(panne);
     }
+
+    public PanneDto getPanne(Integer id){
+        Optional<Panne>panne1 = panneRepo.findById(id);
+        if(panne1.isPresent())
+            return PanneDto.toDto(panne1.get());
+        else
+            return null;
+    }
+
 }
