30434265ecd4c60833b01ea95687ae30dda2ba65
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-08 10:27:14 +0100

    Ajouter des logique de création d'unticket

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Service/TicketSerImpl.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Service/TicketSerImpl.java
@@ -12,28 +12,9 @@ public class TicketSerImpl implements  TicketsService{
     TicketRepository ticketRepository;
 
     @Override
-    public TicketSupportDto addTicketSupport(TicketSupport ticketSupport) {
-        // Créer une nouvelle instance de TicketSupport
-        TicketSupport ticketSupport1 = new TicketSupport();
-
-        // Copier les propriétés nécessaires
-        ticketSupport1.setDescription(ticketSupport.getDescription());
-
-        // Si vous avez déjà un objet Panne, vous pouvez le définir directement
-        if (ticketSupport.getPanne() != null) {
-            ticketSupport1.setPanne(ticketSupport.getPanne());
-        }
-        ticketSupport1.setDescription(ticketSupport.getDescription());
-        ticketSupport1.setEtat(ticketSupport.getEtat());
-
-        TicketSupport savedTicketSupport = ticketRepository.save(ticketSupport1);
-
-
-        TicketSupportDto savedTicketSupportDto = TicketSupportDto.toDto(savedTicketSupport);
-
-        return savedTicketSupportDto;
-
-
+    public TicketSupport addTicketSupport(TicketSupport ticketSupport) {
+        ticketSupport.setDate_creation(ticketSupport.getDate_creation().atStartOfDay().toLocalDate());
+        return this.ticketRepository.save(ticketSupport);
     }
 
     @Override
