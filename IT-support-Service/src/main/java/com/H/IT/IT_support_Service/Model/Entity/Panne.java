659a617e28705a7cce77515c0605f9d60baec45c
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-08 10:28:36 +0100

    Ajouter des Modifications sur les relation

--- IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Model/Entity/Panne.java
+++ IT-support-Service/src/main/java/com/H/IT/IT_support_Service/Model/Entity/Panne.java
@@ -1,5 +1,6 @@
 package com.H.IT.IT_support_Service.Model.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -11,20 +12,17 @@ import java.util.List;
 @Getter @Setter
 public class Panne {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private int id_panne;
-        private String Type;
-        private String description;
-        private Date dateDeclaration;
-        private Date dateReparation;
-        private String etat;
+    private int id_panne;
+    private String Type;
+    private String description;
+    private Date dateDeclaration;
+    private Date dateReparation;
+    private String etat;
 
-
-        @ManyToMany
-        List<Equipement> equipements;
-
-    @OneToMany(mappedBy = "panne")
-    private List<HistoriquesPannes> historiquesPannes;
+    @ManyToOne
+    private Equipement equipement;
 
     @OneToMany(mappedBy = "panne")
+    @JsonIgnore
     private List<TicketSupport> tickets;
 }
